dd0d6b06c656e11bbf120b459ee622a167d14290
Tamerlan <Tamerlan.musaev@mail.ru>
2022-06-10 04:04:38 +0300

    asdasd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,14 @@ import { useNavigate } from "react-router-dom";
 const Footer = ({ page, setPage }) => {
   const navigate = useNavigate();
 
+  const pageLists = [1, 2, 3];
+  const [selectedPage, setSelectedPage] = React.useState(1);
+
   const handleMinus = () => {
     if (page) {
       setPage(page - 10);
       navigate(`/skip=${page - 10}`);
+      setSelectedPage((prev) => prev - 1);
     }
   };
 
@@ -15,12 +19,30 @@ const Footer = ({ page, setPage }) => {
     if (page !== 20) {
       setPage(page + 10);
       navigate(`/skip=${page + 10}`);
+      setSelectedPage((prev) => prev + 1);
     }
+  };
+
+  const handleNavigate = (el) => {
+    setSelectedPage(el);
   };
 
   return (
     <footer>
       <button onClick={handleMinus}>Назад</button>
+      <ul>
+        {pageLists.map((el) => {
+          return (
+            <li
+              onClick={() => handleNavigate(el)}
+              className={el === selectedPage ? "active" : ""}
+              key={el}
+            >
+              {el}
+            </li>
+          );
+        })}
+      </ul>
       <button onClick={handlePlus}>Далее</button>
     </footer>
   );
